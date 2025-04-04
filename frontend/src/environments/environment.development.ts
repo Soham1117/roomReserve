2a0e7d213717b70dc66ddfabf3fe98155d1d505e
Soham <sohampatel1117@gmail.com>
2025-04-03 23:00:02 -0400

    Removed environments

--- frontend/src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000/api/v1', // Point to local Django dev server API v1 root
-  googleMapsApiKey: 'AIzaSyCJVB7jgaJKjkV2qkH0KkUFCiyNVdGdbdo', // Your provided API key
-};
