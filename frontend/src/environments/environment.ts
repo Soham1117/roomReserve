c7774c14d5a240cf4abcd1f353077000b1f61cf6
Soham <sohampatel1117@gmail.com>
2025-04-04 02:29:11 -0400

    Removed environment.ts

--- frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`. (Note: We'll use environment.development.ts)
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false, // Default should be non-production
-  apiUrl: 'http://localhost:8000/api/v1', // Default API URL (can be local)
-  googleMapsApiKey: '', // Default/Placeholder API Key (SHOULD NOT BE COMMITTED if sensitive)
-};
