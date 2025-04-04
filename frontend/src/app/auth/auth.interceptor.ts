bfee56c587e54dc0bdad0d885706331a06e44570
Soham <sohampatel1117@gmail.com>
2025-04-04 02:35:40 -0400

    Updated .gitignore

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment'; // Import environment
 
@@ -12,10 +7,7 @@ import { environment } from '../../environments/environment'; // Import environm
 export class AuthInterceptor implements HttpInterceptor {
   constructor() {} // Inject AuthService if needed for more complex logic
 
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // Get the token from localStorage (or from AuthService state)
     const token = localStorage.getItem('accessToken');
     const isApiUrl = request.url.startsWith(environment.apiUrl); // Check if request is to our API
