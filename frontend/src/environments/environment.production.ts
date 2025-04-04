ccbea80cbfb6779f43775c12887811864ddf5a63
Soham <sohampatel1117@gmail.com>
2025-04-04 02:20:41 -0400

    Added backend url

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://54.165.252.248/api/v1', // Placeholder - UPDATE IF NEEDED
+  googleMapsApiKey: '__API_KEY_PLACEHOLDER__', // Placeholder to be replaced by build script
+};
