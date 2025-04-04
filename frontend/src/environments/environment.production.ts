4b312d38939863745696e3ff64449e4b8da74c9b
Soham <sohampatel1117@gmail.com>
2025-04-04 02:26:25 -0400

    Updated .gitignore.

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://54.165.252.248/api/v1', // Placeholder - UPDATE IF NEEDED
+  apiUrl: 'http://54.165.252.248/api/v1', // Updated API URL for EC2
   googleMapsApiKey: '__API_KEY_PLACEHOLDER__', // Placeholder to be replaced by build script
 };
