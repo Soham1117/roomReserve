8c18051ae34d36e2e32c39943fc2f0188487c6ff
Soham <sohampatel1117@gmail.com>
2025-04-06 21:55:12 -0400

    Untrack backend directory

--- backend/roomReserveBackend/auth_service/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Role
-# Register your models here.
-
-# If using custom Role model instead of Django Groups
-# admin.site.register(Role)
-
-# Note: Django's User and Group models are already registered by default
-# in django.contrib.auth.admin
